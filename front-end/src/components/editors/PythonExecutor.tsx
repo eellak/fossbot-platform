c531092280485dbb9f174b0e7595c07c1b4d918a
Eleftheria <eleftheria@crowdpolicy.com>
2024-01-12 11:15:16 +0200

    format files

--- front-end/src/components/editors/PythonExecutor.tsx
+++ front-end/src/components/editors/PythonExecutor.tsx
@@ -1,60 +1,59 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 type PythonExecutorProps = {
-    pythonScript: string;
-    onRunScript: (runScript: () => Promise<void>) => void;
+  pythonScript: string;
+  onRunScript: (runScript: () => Promise<void>) => void;
 };
 
 const PythonExecutor: React.FC<PythonExecutorProps> = ({ pythonScript, onRunScript }) => {
-    const [results, setResults] = useState<string[]>([]);
+  const [results, setResults] = useState<string[]>([]);
 
-    // Create a new web worker
-    const pyodideWorker = new Worker(new URL('../../workers/pyodideWorker.ts', import.meta.url));
+  // Create a new web worker
+  const pyodideWorker = new Worker(new URL('../../workers/pyodideWorker.ts', import.meta.url));
 
-    // Set up event listener for messages from the worker
-    pyodideWorker.onmessage = function (event: MessageEvent<string>) {
-        console.log('Received result from worker:', event.data);
+  // Set up event listener for messages from the worker
+  pyodideWorker.onmessage = function (event: MessageEvent<string>) {
+    console.log('Received result from worker:', event.data);
 
-        if(event.data.includes('CMD:')) {
-          let result = event.data.split('CMD:')[1];
-          setResults(prevResults => [...prevResults, result]);
-        }
+    if (event.data.includes('CMD:')) {
+      let result = event.data.split('CMD:')[1];
+      setResults((prevResults) => [...prevResults, result]);
+    }
 
-        if(event.data == 'EMPTY_RESULTS') { 
-          setResults([]);
-        }
+    if (event.data == 'EMPTY_RESULTS') {
+      setResults([]);
+    }
+  };
+
+  useEffect(() => {
+    //Set up socket connection and pyodide
+    pyodideWorker.postMessage('SETUP');
+
+    return () => {
+      // Cleanup socket.io connection
+      pyodideWorker.postMessage('CLOSE');
     };
+  }, []);
 
-    useEffect(() => {
-        //Set up socket connection and pyodide
-        pyodideWorker.postMessage('SETUP');
+  const runPythonScript = useCallback(async () => {
+    if (pythonScript == '') {
+      alert('Please write a command in the Monaco Editor!');
+      return;
+    }
+    pyodideWorker.postMessage(JSON.stringify(pythonScript));
+  }, [pythonScript]);
 
-        return () => {
-            // Cleanup socket.io connection
-            pyodideWorker.postMessage('CLOSE');
-        };
-    }, []);
+  useEffect(() => {
+    onRunScript(runPythonScript);
+  }, [runPythonScript, onRunScript]);
 
-    const runPythonScript = useCallback(async () => {
-      if(pythonScript == '') {
-        alert('Please write a command in the Monaco Editor!');
-        return;
-      }
-      pyodideWorker.postMessage(JSON.stringify(pythonScript));
-    }, [pythonScript]);
-  
-    
-    useEffect(() => {
-      onRunScript(runPythonScript);
-    }, [runPythonScript, onRunScript]);
-  
-    return (
-        <div>
-            {results.map((result, index) => (
-                <p key={index}>{result}</p>
-            ))}
-        </div>
-    );
+  return (
+    <div>
+      {results.map((result, index) => (
+        <p key={index}>{result}</p>
+      ))}
+    </div>
+  );
 };
 
 export default PythonExecutor;
--- front-end/src/workers/pyodideWorker.ts
+++ front-end/src/workers/pyodideWorker.ts
@@ -1,145 +1,142 @@
 let socket: any;
-let pyodide: any; 
+let pyodide: any;
 
 function setSocket(value: any) {
-    socket = value;
+  socket = value;
 }
 
 function setPyodide(value: any) {
-    pyodide = value;
+  pyodide = value;
 }
 
 onmessage = async function (event: MessageEvent<string>) {
-    console.log("Received message from the main thread:", event.data);
+  console.log('Received message from the main thread:', event.data);
 
-    if(event.data == 'SETUP') {
-        await setUpSocket();
-        if(socket)
-            pyodide = await setUpPyodide(socket);
+  if (event.data == 'SETUP') {
+    await setUpSocket();
+    if (socket) pyodide = await setUpPyodide(socket);
 
-        console.log('SET UP READY');
-    } else if (event.data == 'CLOSE') {
-        closeSocket()
-        console.log('SOCKET WAS JUST CLOSED');
-    } else {
-        console.log('Lets run command '+event.data);
-        const res = await runPythonCode(event.data)
-    } 
-}
+    console.log('SET UP READY');
+  } else if (event.data == 'CLOSE') {
+    closeSocket();
+    console.log('SOCKET WAS JUST CLOSED');
+  } else {
+    console.log('Lets run command ' + event.data);
+    const res = await runPythonCode(event.data);
+  }
+};
 
 const setUpSocket = async () => {
-    // Function to establish WebSocket connection
-    const socketIOClient = await import("socket.io-client");
-    const newSocket = socketIOClient.io('http://localhost:5000/godot');
+  // Function to establish WebSocket connection
+  const socketIOClient = await import('socket.io-client');
+  const newSocket = socketIOClient.io('http://localhost:5000/godot');
 
-    newSocket.on('connect', () => {
-        console.log('Socket.io connected');
-        setSocket(newSocket);
-    });
-
-    newSocket.on('connect_error', (error) => {
-        console.error('Socket.io connection error:', error);
-    })
-
-    newSocket.on('message', (message) => {
-        console.log('Received message:', message);
-        // Handle incoming messages
-    });
-    newSocket.on('disconnect', () => {
-        console.log('Socket.io disconnected');
-    });
+  newSocket.on('connect', () => {
+    console.log('Socket.io connected');
     setSocket(newSocket);
-}
+  });
 
-const setUpPyodide = async (socket: any) => { 
-    //Inintialize Pyodide
-    const pyodideModule = await import('pyodide');
-    const loadedPyodide = await pyodideModule.loadPyodide({
-        indexURL: "https://cdn.jsdelivr.net/pyodide/v0.23.4/full/",
-    });
+  newSocket.on('connect_error', (error) => {
+    console.error('Socket.io connection error:', error);
+  });
 
-    loadedPyodide.setStdout({ batched: (msg: string) => postMessage('CMD:'+ msg) });
+  newSocket.on('message', (message) => {
+    console.log('Received message:', message);
+    // Handle incoming messages
+  });
+  newSocket.on('disconnect', () => {
+    console.log('Socket.io disconnected');
+  });
+  setSocket(newSocket);
+};
 
-    loadedPyodide.setStderr({ batched: (msg: string) => postMessage('CMD:'+ msg) });
+const setUpPyodide = async (socket: any) => {
+  //Inintialize Pyodide
+  const pyodideModule = await import('pyodide');
+  const loadedPyodide = await pyodideModule.loadPyodide({
+    indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.23.4/full/',
+  });
 
-    const Fossbot = await import("../components/editors/RobotJS");
-    const robot = new Fossbot.default(socket, "fossbot", "1");
+  loadedPyodide.setStdout({ batched: (msg: string) => postMessage('CMD:' + msg) });
 
-    try {
-        // Use micropip to install Python packages
-        // await loadedPyodide.loadPackage('https://files.pythonhosted.org/packages/f8/bf/4790ed063ca2daa58fb20285fc3707218cf01e174209355d081d83094f6d/python_socketio-5.10.0-py3-none-any.whl'); 
-        
-        // Expose JS methods to Python             
-        loadedPyodide.globals.set('robot', robot);
+  loadedPyodide.setStderr({ batched: (msg: string) => postMessage('CMD:' + msg) });
 
-        //Custom Modules
-        const moduleResponse = await fetch('/fossbotlib/testlib.py'); // Relative path from the public folder
-        const moduleText = await moduleResponse.text();
-        //loadedPyodide.runPython(moduleText);                
-        // Write the module text to Pyodide's virtual file system
-        loadedPyodide.FS.writeFile('/home/pyodide/testlib.py', moduleText);
-    } catch (e) {
-        console.error("Error loading module or installing package:", e);
-    }
+  const Fossbot = await import('../components/editors/RobotJS');
+  const robot = new Fossbot.default(socket, 'fossbot', '1');
 
-    return loadedPyodide;
-}
+  try {
+    // Use micropip to install Python packages
+    // await loadedPyodide.loadPackage('https://files.pythonhosted.org/packages/f8/bf/4790ed063ca2daa58fb20285fc3707218cf01e174209355d081d83094f6d/python_socketio-5.10.0-py3-none-any.whl');
+
+    // Expose JS methods to Python
+    loadedPyodide.globals.set('robot', robot);
+
+    //Custom Modules
+    const moduleResponse = await fetch('/fossbotlib/testlib.py'); // Relative path from the public folder
+    const moduleText = await moduleResponse.text();
+    //loadedPyodide.runPython(moduleText);
+    // Write the module text to Pyodide's virtual file system
+    loadedPyodide.FS.writeFile('/home/pyodide/testlib.py', moduleText);
+  } catch (e) {
+    console.error('Error loading module or installing package:', e);
+  }
+
+  return loadedPyodide;
+};
 
 const closeSocket = () => {
-    if (socket && socket.connected) {
-        socket.disconnect();
-        return true;
-    }
-}
+  if (socket && socket.connected) {
+    socket.disconnect();
+    return true;
+  }
+};
 
 const runPythonCode = async (pythonScript: string) => {
-    console.log("runPythonCode..");
+  console.log('runPythonCode..');
 
-    if (!pyodide) { 
-        console.log("Pyodide not already loaded ..");
-        pyodide = await setUpPyodide(socket);
-    } 
+  if (!pyodide) {
+    console.log('Pyodide not already loaded ..');
+    pyodide = await setUpPyodide(socket);
+  }
 
-    console.log("Pyodide okay ..  pyodide:", pyodide);
+  console.log('Pyodide okay ..  pyodide:', pyodide);
 
+  if (pythonScript) {
+    console.log('Running Python script :', pythonScript);
 
-        if(pythonScript) {
-            console.log("Running Python script :", pythonScript);
+    postMessage('EMPTY_RESULTS');
 
-           postMessage('EMPTY_RESULTS')
+    // Adds await to some function
+    //const finalScript = fix_awaits(pythonScript);
+    const finalScript = JSON.parse(pythonScript);
+    console.log('finalScript :', finalScript);
 
-            // Adds await to some function
-            //const finalScript = fix_awaits(pythonScript);
-            const finalScript = JSON.parse(pythonScript);
-            console.log("finalScript :", finalScript);
+    try {
+      await pyodide.runPythonAsync(finalScript);
+    } catch (e: any) {
+      if (e.constructor.name === 'PythonError') {
+        const errorMessage = e.message;
+        const errorStartIndex = errorMessage.indexOf('File "<exec>"');
+        let formattedErrorMessage = '';
 
-            try {
-                await pyodide.runPythonAsync(finalScript);
-            } catch (e: any) {
-                if (e.constructor.name === "PythonError") {
-                const errorMessage = e.message;
-                const errorStartIndex = errorMessage.indexOf('File "<exec>"');
-                let formattedErrorMessage = "";
-
-                if (errorStartIndex !== -1) {
-                    // Extracting the error message starting after '^^^^'
-                    formattedErrorMessage += errorMessage.substring(errorStartIndex);
-                } else {
-                    // If '^^^^' not found, use the entire message
-                    formattedErrorMessage += errorMessage;
-                }
-                    // Append the formatted error message to the results
-                    postMessage('CMD:'+ formattedErrorMessage)
-                } else {
-                // For non-Python errors, you might still want to handle them differently
-                console.error("Unexpected error:", e);
-                }
-            }
+        if (errorStartIndex !== -1) {
+          // Extracting the error message starting after '^^^^'
+          formattedErrorMessage += errorMessage.substring(errorStartIndex);
+        } else {
+          // If '^^^^' not found, use the entire message
+          formattedErrorMessage += errorMessage;
         }
-}
+        // Append the formatted error message to the results
+        postMessage('CMD:' + formattedErrorMessage);
+      } else {
+        // For non-Python errors, you might still want to handle them differently
+        console.error('Unexpected error:', e);
+      }
+    }
+  }
+};
 
 const fix_awaits = (code: string) => {
-
-    const result = code.replace('robot.forward(', 'await robot.forward(');
-    return result;
-}
+  const result = code.replace('robot.forward(', 'await robot.forward(');
+  return result;
+};
