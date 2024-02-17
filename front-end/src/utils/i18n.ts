0c2d9c1751dc2e18a6b86d532b7d43a30ea5a575
Eleftheria Papageorgiou <it2754@hua.gr>
2024-02-17 23:48:09 +0200

    i18 set up for greek

--- front-end/src/utils/i18n.ts
+++ front-end/src/utils/i18n.ts
@@ -1,21 +1,24 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import english from './languages/en.json';
+import greek from './languages/gr.json';
 
 const resources = {
-  en: {
-    translation: english,
-  }
+    en: {
+        translation: english,
+    },
+    gr: {
+        translation: greek,
+    },
 };
 
-i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .init({
-    resources,
-    lng: 'en',
-    interpolation: {
-      escapeValue: false, // react already safes from xss
-    },
-  });
+i18n.use(initReactI18next) // passes i18n down to react-i18next
+    .init({
+        resources,
+        lng: 'en',
+        interpolation: {
+            escapeValue: false, // react already safes from xss
+        },
+    });
 
 export default i18n;
