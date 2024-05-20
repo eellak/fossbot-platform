59477dcf5d3073b2a5e837dade8d5c9e75045a1d
Eleftheria Papageorgiou <it21754@hua.gr>
2024-05-21 02:08:56 +0300

    added right column with cards

--- front-end/src/components/forms/form-horizontal/AccountSettingsForm.tsx
+++ front-end/src/components/forms/form-horizontal/AccountSettingsForm.tsx
@@ -5,7 +5,7 @@ import SuccessAlert from 'src/components/alerts/SuccessAlert';
 import ErrorAlert from 'src/components/alerts/ErrorAlert';
 import ChangePassword from 'src/components/alerts/ChangePassword';
 import { IconMail, IconUser } from '@tabler/icons-react';
-import { Grid, InputAdornment, Button } from '@mui/material';
+import { Grid, InputAdornment, Button, Typography, Card, CardContent } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useAuth } from "src/authentication/AuthProvider";
 
@@ -58,92 +58,140 @@ const AccountSettingsForm = ({ user }: AccountSettingsFormType) => {
 
   return (
     <div>
-      {/* ------------------------------------------------------------------------------------------------ */}
-      {/* Basic Form Layout for Account Settings */}
-      {/* ------------------------------------------------------------------------------------------------ */}
-      <Grid container spacing={3} xs={12} sm={12} md={10} lg={8} xl={8} alignItems="center">
-        {/* 1 */}
-        <Grid item xs={12} sm={3} display="flex" alignItems="center">
-          <CustomFormLabel htmlFor="bi-firstname" sx={{ mt: 0, mb: { xs: '-10px', sm: 0 } }}>
-            {t('firstname')}
-          </CustomFormLabel>
+      <Grid container spacing={3}>
+        {/* Left half with form inputs */}
+        <Grid item xs={12} md={6}>
+          <Grid container spacing={3} paddingLeft={5}>
+            <Grid item xs={12} sm={3} display="flex" alignItems="center">
+              <CustomFormLabel htmlFor="bi-firstname" sx={{ mt: 0, mb: { xs: '-10px', sm: 0 } }}>
+                {t('firstname')}
+              </CustomFormLabel>
+            </Grid>
+            <Grid item xs={12} sm={9}>
+              <CustomOutlinedInput
+                id="bi-firstname"
+                name="firstname"
+                placeholder="John"
+                fullWidth
+                value={formData.firstname}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={3} display="flex" alignItems="center">
+              <CustomFormLabel htmlFor="bi-lastname" sx={{ mt: 0, mb: { xs: '-10px', sm: 0 } }}>
+                {t('lastname')}
+              </CustomFormLabel>
+            </Grid>
+            <Grid item xs={12} sm={9}>
+              <CustomOutlinedInput
+                id="bi-lastname"
+                name="lastname"
+                placeholder="Deo"
+                fullWidth
+                value={formData.lastname}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={3} display="flex" alignItems="center">
+              <CustomFormLabel htmlFor="bi-username" sx={{ mt: 0, mb: { xs: '-10px', sm: 0 } }}>
+                {t('username')}
+              </CustomFormLabel>
+            </Grid>
+            <Grid item xs={12} sm={9}>
+              <CustomOutlinedInput
+                id="bi-username"
+                name="username"
+                placeholder="john.deo"
+                fullWidth
+                value={formData.username}
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={3} display="flex" alignItems="center">
+              <CustomFormLabel htmlFor="bi-email" sx={{ mt: 0, mb: { xs: '-10px', sm: 0 } }}>
+                {t('password')}
+              </CustomFormLabel>
+            </Grid>
+            <Grid item xs={12} sm={9}>
+              <Button variant="contained" color="primary" onClick={showChangeYourPassswordSnackbar}>
+                {t('changeYourPassword')}
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={3} display="flex" alignItems="center">
+              <CustomFormLabel htmlFor="bi-emailAddress" sx={{ mt: 0, mb: { xs: '-10px', sm: 0 } }}>
+                {t('emailAddress')}
+              </CustomFormLabel>
+            </Grid>
+            <Grid item xs={12} sm={9}>
+              <CustomOutlinedInput
+                id="bi-email"
+                placeholder="email@example.com"
+                fullWidth
+                value={formData.email}
+                name="email"
+                onChange={handleInputChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={3}></Grid>
+            <Grid item xs={12} sm={9} mt={5}>
+              <Button variant="contained" color="primary" onClick={handleFormSubmit}>
+                {t('update')}
+              </Button>
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={9}>
-          <CustomOutlinedInput
-            id="bi-firstname"
-            name="firstname"
-            placeholder="John"
-            fullWidth
-            value={formData.firstname}
-            onChange={handleInputChange}
-          />
-        </Grid>
-        {/* 2 */}
-        <Grid item xs={12} sm={3} display="flex" alignItems="center">
-          <CustomFormLabel htmlFor="bi-lastname" sx={{ mt: 0, mb: { xs: '-10px', sm: 0 } }}>
-            {t('lastname')}
-          </CustomFormLabel>
-        </Grid>
-        <Grid item xs={12} sm={9}>
-          <CustomOutlinedInput
-            id="bi-lastname"
-            name="lastname"
-            placeholder="Deo"
-            fullWidth
-            value={formData.lastname}
-            onChange={handleInputChange}
-          />
-        </Grid>
-        {/* 3 */}
-        <Grid item xs={12} sm={3} display="flex" alignItems="center">
-          <CustomFormLabel htmlFor="bi-username" sx={{ mt: 0, mb: { xs: '-10px', sm: 0 } }}>
-            {t('username')}
-          </CustomFormLabel>
-        </Grid>
-        <Grid item xs={12} sm={9}>
-          <CustomOutlinedInput
-            id="bi-username"
-            name="username"
-            placeholder="john.deo"
-            fullWidth
-            value={user?.username}
-            onChange={handleInputChange}
-          />
-        </Grid>
-        {/* 4 */}
-        <Grid item xs={12} sm={3} display="flex" alignItems="center">
-          <CustomFormLabel htmlFor="bi-email" sx={{ mt: 0, mb: { xs: '-10px', sm: 0 } }}>
-            {t('password')}
-          </CustomFormLabel>
-        </Grid>
-        <Grid item xs={12} sm={9}>
-          <Button variant="contained" color="primary" onClick={showChangeYourPassswordSnackbar}>
-            {t('changeYourPassword')}
-          </Button>
-        </Grid>
-        {/* 5 */}
-        <Grid item xs={12} sm={3} display="flex" alignItems="center">
-          <CustomFormLabel htmlFor="bi-emailAddress" sx={{ mt: 0, mb: { xs: '-10px', sm: 0 } }}>
-            {t('emailAddress')}
-          </CustomFormLabel>
-        </Grid>
-        <Grid item xs={12} sm={9}>
-          <CustomOutlinedInput
-            id="bi-email"
-            placeholder="email@example.com"
-            fullWidth
-            value={formData.email}
-            name="email"
-            onChange={handleInputChange}
-          />
-        </Grid>
-        <Grid item xs={12} sm={3}></Grid>
-        <Grid item xs={12} sm={9} mt={5}>
-          <Button variant="contained" color="primary" onClick={handleFormSubmit}>
-            {t('update')}
-          </Button>
+
+        {/* Right half with additional rows */}
+        <Grid item xs={12} md={6} spacing={3}>
+          {/* First row */}
+          <Grid container spacing={3} paddingRight={5}>
+            <Grid item xs={12} sm={5}>
+              <Card>
+                <CardContent>
+                  <Typography variant="h6" fontWeight="bold" gutterBottom>
+                    Title 1
+                  </Typography>
+                  <Typography variant="body1">
+                    Your text for card 1 here
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={5}>
+              <Card>
+                <CardContent>
+                  <Typography variant="h6" fontWeight="bold" gutterBottom>
+                    Title 2
+                  </Typography>
+                  <Typography variant="body1">
+                    Your text for card 2 here
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+
+          {/* Second row */}
+          <Grid container spacing={2} paddingRight={5} sx={{ marginTop: 2 }}>
+            <Grid item xs={12} sm={5}>
+              <Card>
+                <CardContent>
+                  <Typography variant="h6" fontWeight="bold" gutterBottom>
+                    Title 3
+                  </Typography>
+                  <Typography variant="body1">
+                    Your text for card 3 here
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
+
+
+      {/* </Grid> */}
+
       {showSuccessAlert && (
         <SuccessAlert title={t('alertMessages.userDataUpdated')} description={""} />
       )}
