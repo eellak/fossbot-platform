cc60868e4767c9fc4cccf82c7fa8236134e7e542
Eleftheria Papageorgiou <it21754@hua.gr>
2024-05-28 01:50:00 +0300

    remove unused

--- front-end/src/views/dashboard/Dashboard.tsx
+++ front-end/src/views/dashboard/Dashboard.tsx
@@ -1,33 +1,21 @@
- 
+
 import React from 'react';
+import PageContainer from 'src/components/container/PageContainer';
+import ProjectsCard from 'src/components/dashboard/ProjectsCard';
 import { Box, Grid } from '@mui/material';
 import { useTranslation } from 'react-i18next';
-
-import PageContainer from 'src/components/container/PageContainer';
-
-import TopCards from 'src/components/dashboards/modern/TopCards';
-import ProjectsCard from 'src/components/dashboards/modern/ProjectsCard';
-import WelcomeHomePage from 'src/layouts/full/shared/welcome/WelcomeHomePage';
 
 const Modern = () => {
   const { t } = useTranslation();
 
   return (
     <PageContainer title={t('dashboard-page.title')} description={t('dashboard-page.description')}>
-
       <Box>
         <Grid container spacing={3}>
-          {/* column */}
-          {/* <Grid item xs={12} lg={12}>
-            <TopCards />
-          </Grid> */}
-          {/* column */}
           <Grid item xs={12} lg={12}>
             <ProjectsCard />
           </Grid>
         </Grid>
-        {/* column */}
-        {/* <WelcomeHomePage /> */}
       </Box>
     </PageContainer>
   );
