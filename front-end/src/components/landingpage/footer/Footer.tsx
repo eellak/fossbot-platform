b0dc483175851340515836ed52fdf19c82685c04
Eleftheria Papageorgiou <it21754@hua.gr>
2024-06-01 01:14:25 +0300

    rotate image in footer

--- front-end/src/components/landingpage/footer/Footer.tsx
+++ front-end/src/components/landingpage/footer/Footer.tsx
@@ -3,15 +3,16 @@ import React from 'react';
 import logoIcon from 'src/assets/images/fossbot/logos-main/bot.png'
 import { Grid, Link, Typography, Container } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import './Footer.css'; 
 
 const Footer = () => {
-  const { t } = useTranslation(); 
+  const { t } = useTranslation();
 
   return (
     <Container maxWidth="lg">
       <Grid container spacing={3} justifyContent="center" mt={4}>
         <Grid item xs={12} sm={5} lg={4} textAlign="center">
-          <img src={logoIcon} alt="icon" width={'50px'} />
+          <img src={logoIcon} alt="icon" width={'50px'} className="rotate-on-hover" />
           <Typography fontSize="16" color="textSecondary" mt={1} mb={4}>
             {t('footer.rights')}
             <Link target="_blank" href="https://hot.dit.hua.gr/">
