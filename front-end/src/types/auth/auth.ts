beff35dd27a73ac13a1265aaa4fcd847d0ec2748
Eleftheria Papageorgiou <it21754@hua.gr>
2024-06-01 14:01:27 +0300

    registerType update

--- front-end/src/types/auth/auth.ts
+++ front-end/src/types/auth/auth.ts
@@ -2,6 +2,8 @@ export interface registerType {
     title?: string;
     subtitle?: JSX.Element | JSX.Element[];
     subtext?: JSX.Element | JSX.Element[];
+    onShowSuccessAlert: (message: string) => void;
+    onShowErrorAlert: (message: string) => void;
   }
   
   export interface loginType {
