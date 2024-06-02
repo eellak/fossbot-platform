2475c3d9b99fc51dbf41de59c71b0fdd43241018
chronis10 <chronis@hua.gr>
2024-06-03 01:23:23 +0300

    landing page banner fix

--- front-end/src/components/landingpage/banner/Banner.tsx
+++ front-end/src/components/landingpage/banner/Banner.tsx
@@ -1,56 +1,35 @@
- 
 import React from 'react';
 import { Grid, Box, Container, useMediaQuery, styled, Stack, Theme } from '@mui/material';
 import BannerContent from './BannerContent';
 import bot from 'src/assets/images/fossbot/logos-main/bot.png';
-import botR from 'src/assets/images/fossbot/logos-main/botR.png';
 import gfoss from 'src/assets/images/fossbot/logos-main/gfoss.png';
 import hua from 'src/assets/images/fossbot/logos-main/hua.png';
-// import bannerbgImg2 from 'src/assets/images/landingpage/fossbot2.jpg';
-// import bannerbgImg3 from 'src/assets/images/landingpage/fossbot3.jpg';
-// import bannerbgImg4 from 'src/assets/images/landingpage/fossbot4.jpg';
-// import bannerbgImg5 from 'src/assets/images/landingpage/fossbot5.jpg';
 
 const Banner = () => {
   const lgUp = useMediaQuery((theme: Theme) => theme.breakpoints.up('lg'));
 
-  const SliderBox = styled(Box)(() => ({
+  const SliderContainer = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    flexWrap: 'wrap',
+    height: '200%',
     '@keyframes slideup': {
       '0%': {
-        transform: 'translate3d(0, 0, 0)',
+        transform: 'translateY(0)',
       },
-      '100% ': {
-        transform: 'translate3d(0px, -100%, 0px)',
+      '100%': {
+        transform: 'translateY(-50%)',
       },
     },
-    
-    animation: 'slideup 35s linear infinite',
+    animation: 'slideup 20s linear infinite',
+  }));
 
+  const SliderBox = styled(Box)(({ theme }) => ({
     img: {
       width: '100%',
       height: 'auto',
       objectFit: 'contain',
     },
   }));
-
-  // const SliderBox2 = styled(Box)(() => ({
-  //   '@keyframes slideDown': {
-  //     '0%': {
-  //       transform: 'translate3d(0, -100%, 0)',
-  //     },
-  //     '100% ': {
-  //       transform: 'translate3d(0px, 0, 0px)',
-  //     },
-  //   },
-    
-  //   animation: 'slideDown 35s linear infinite',
-
-  //   img: {
-  //     width: '100%',
-  //     height: '20%',
-  //     objectFit: 'contain',
-  //   },
-  // }));
 
   const images = [
     { src: hua, alt: 'banner' },
@@ -63,7 +42,6 @@ const Banner = () => {
   for (let i = 0; i < 10; i++) {
     repeatedData = repeatedData.concat(images);
   }
- 
 
   return (
     <Box mb={10} sx={{ overflow: 'hidden' }}>
@@ -81,22 +59,26 @@ const Banner = () => {
                   minWidth: '70%',
                   height: 'calc(100vh - 100px)',
                   maxHeight: '790px',
+                  overflow: 'hidden',
+                  position: 'relative',
                 }}
               >
-                <Stack direction={'row'}>
-                  <Grid container spacing={2}>
+                <SliderContainer>
                   {repeatedData.map((repeatedData, index) => (
-                  <Grid item xs={3}>
+                    <Grid item xs={3} key={index} sx={{ padding: '8px' }}>
                       <SliderBox width={"100px"}>
                         <img src={repeatedData.src} alt={repeatedData.alt} />
                       </SliderBox>
-                      
                     </Grid>
                   ))}
-
-
-                  </Grid>
-                </Stack>
+                  {repeatedData.map((repeatedData, index) => (
+                    <Grid item xs={3} key={`repeat-${index}`} sx={{ padding: '8px' }}>
+                      <SliderBox width={"100px"}>
+                        <img src={repeatedData.src} alt={repeatedData.alt} />
+                      </SliderBox>
+                    </Grid>
+                  ))}
+                </SliderContainer>
               </Box>
             </Grid>
           ) : null}
