e15f0d949c21836b0396890e55335378c2180922
chronis10 <chronis@hua.gr>
2024-06-09 17:28:07 +0300

    matomo config updates

--- front-end/src/components/matomo-tracker/MatomoTracker.tsx
+++ front-end/src/components/matomo-tracker/MatomoTracker.tsx
@@ -10,26 +10,44 @@ declare global {
 
 const MatomoTracker = () => {
   const location = useLocation();
+  const MATOMO_URL = 'fossbot.gr/analytics'; // Replace with your Matomo URL
+  const CONTAINER_ID = 'NY6wzURV'; // Replace with your container ID
 
   useEffect(() => {
     // Function to inject Matomo script
     const injectMatomo = () => {
-      var _mtm = window._mtm = window._mtm || [];
-      _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
-      (function() {
-        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.async=true; g.src='https://fossbot.gr/analytics/js/containerc_NY6wzURV.js'; s.parentNode.insertBefore(g,s);
-      })();
+      const scriptContent = `
+        window._mtm = window._mtm || [];
+        window._mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+        (function() {
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.type='text/javascript'; g.async=true; g.src='https://${MATOMO_URL}/js/container_${CONTAINER_ID}.js'; s.parentNode.insertBefore(g,s);
+        })();
+      `;
+
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.async = true;
+      script.innerHTML = scriptContent;
+      document.head.appendChild(script);
     };
 
-    // Initialize _mtm array if it doesn't exist
-    if (!window._mtm) {
-      window._mtm = [];
+    // Inject the Matomo script if not already injected
+    if (!window._mtm || window._mtm.length === 0) {
       injectMatomo();
     }
 
-    // Track page view on route change
-    window._mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.PageView', 'url': window.location.href});
+    // Ensure that Matomo script is loaded and _mtm is defined
+    if (window._mtm) {
+      // Track page view on route change
+      window._mtm.push({
+        'mtm.startTime': (new Date().getTime()), 
+        'event': 'mtm.PageView', 
+        'url': window.location.pathname + window.location.search + window.location.hash
+      });
+    } else {
+      console.error('Matomo script not loaded properly.');
+    }
   }, [location]);
 
   return null;
