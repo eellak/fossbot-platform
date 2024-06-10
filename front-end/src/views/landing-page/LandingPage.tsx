f0ce7acbb4314fcfaf28a590cfc60bba3378669e
Eleftheria Papageorgiou <it21754@hua.gr>
2024-06-11 00:57:20 +0300

    landing page laways light theme

--- front-end/src/views/landing-page/LandingPage.tsx
+++ front-end/src/views/landing-page/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PageContainer from 'src/components/container/PageContainer';
 import Banner from 'src/components/landingpage/banner/Banner';
 import AboutFossbot from 'src/components/landingpage/colorContainerFossbot/AboutFossbot';
@@ -7,19 +7,27 @@ import Footer from 'src/components/landingpage/footer/Footer';
 import LpHeader from 'src/components/landingpage/header/Header';
 import PlatformCard from 'src/components/landingpage/card_fossbot/CardPlatform';
 import FossbotCard from 'src/components/landingpage/card_fossbot/CardFossbot';
+import { AppDispatch } from 'src/store/Store';
+import { useDispatch } from 'react-redux';
+import { setDarkMode } from 'src/store/customizer/CustomizerSlice';
 import { useTranslation } from 'react-i18next';
 
 const Landingpage = () => {
   const { t } = useTranslation();
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(setDarkMode('light'));
+  }, [dispatch]);
 
   return (
     <PageContainer title={t('landing-title.title')} description={t('landing-title.description')}>
       <LpHeader />
       <Banner />
-      {/* <FossbotCard /> */} 
+      {/* <FossbotCard /> */}
       <AboutFossbot />
       <FossbotCard />
-      <AboutPlatform />      
+      <AboutPlatform />
       <PlatformCard />
       {/* <C2a2 /> */}
       {/* <DemoSlider /> */}
